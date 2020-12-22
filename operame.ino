5687e044408eccc618b7a870a31a1089927416e1
Juerd Waalboer <juerd@tnx.nl>
2020-12-22 05:19:51 +0100

    Don't publish invalid sensor readings to MQTT

--- operame.ino
+++ operame.ino
@@ -363,6 +363,7 @@ void loop() {
         mqtt_interval,
         [] {
             if (!mqtt_enabled) return;
+            if (co2 <= 0) return;
             connect_mqtt();
             String message = mqtt_template;
             message.replace("{}", String(co2));
